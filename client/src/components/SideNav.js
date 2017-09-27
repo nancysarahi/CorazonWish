cc57a2fb83bd3bc116c7d62d0f5881eb33eb9929
Nancy Montiel <nancysarahi@Nancys-MacBook-Pro.local>
2017-09-26 20:20:41 -0500

    works again

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -5,21 +5,21 @@ function SideNav() {
   return (
     <div className="collapse navbar-collapse navbar-ex1-collapse">
       <ul className="nav navbar-nav side-nav">
-        <div className="active">
+        <li className="active">
           <Link to="/"> <i className="fa fa-fw fa-dashboard" />
             Home
           </Link>
-        </div>
-        <div>
+        </li>
+        <li>
           <Link to="/ListOfCats">
             <i className="fa fa-fw fa-bar-chart-o" /> List of Cats
           </Link>
-        </div>
-        <div>
+        </li>
+        <li>
           <Link to="/CreateCat">
             <i className="fa fa-fw fa-table" /> Create Cat
           </Link>
-        </div>
+        </li>
       </ul>
     </div>);
 }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,5 @@
 import {combineReducers} from "redux";
 
-// function cats(state = "", action) { 
-//   return state;
-// }
-
 function cats(state = [], action) {
   if (action.type === "MY_CAT_LIST_LOADED"){
     return action.value
